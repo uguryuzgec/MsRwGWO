485a4732396d22b912972b383bddbf14b234424e
Uğur Yüzgeç <uyuzgec@gmail.com>
2021-03-30 20:36:19 +0300

    Update main.m

--- main.m
+++ main.m
@@ -1,9 +1,9 @@
 %   A Multi-strategy Random weighted Gray Wolf Optimizer            %
-%                       (MsRwGWO) -Analiz-                          %
-%																	%
+%                       (MsRwGWO)                                   %
+%																                                   	%
 %       A Multi-strategy Random weighted Gray Wolf Optimizer        %
 %           for short-term wind speed forecasting                   %
-%          Tufan İnac, Emrah Dokur & Ugur Yuzgec                   %
+%          Tufan Inac, Emrah Dokur & Ugur Yuzgec                    %
 
 clear 
 close all
@@ -14,9 +14,9 @@ clc
 % 1-3 unimodal func.
 % 4-16 multimodal func.
 % 23-28 composition func.
-func_num=23; % fonk sayisi
-runs=1; % tekrar sayisi
-D=2; % boyut sayisi
+func_num=23; 
+runs=1; 
+D=2; % number of dimension
 Xmin=-100;
 Xmax=100;
 
@@ -29,45 +29,27 @@ empty_solution.t=[];
 solution=repmat(empty_solution,func_num,runs);
 lb=Xmin;
 ub=Xmax;
-X_suru=lb+(ub-lb).*rand(pop_size,D);
+X_suru=lb+(ub-lb).*rand(pop_size,D); % initial population
 
-[gbest,gbestval,FES,t] = GWO_func(fhd,D,pop_size,iter_max,Xmin,Xmax,X_suru,func_num);
+% Optimization with MsRwGWO
+[gbest,gbestval,FES,t] = MsRwGWO_func(fhd,D,pop_size,iter_max,Xmin,Xmax,X_suru,func_num);
 solution(func_num,1).position = gbest;
 solution(func_num,1).cost = abs(gbestval-func_num*100);
 solution(func_num,1).t = t;
 fprintf('\n---------------------------------------------------------------\n');
-fprintf('Optimization with GWO \n');
+fprintf('Optimization with MsRwGWO \n');
 fprintf('Func no: %d -> %d. run : best error = %1.2e\n',func_num,runs,solution(func_num,1).cost);
 
-% % %  plot_function
+plot_function
 drawnow
-% 1. Convergence Analysis
-% istenilen fonksiyonun (convergence curve) yakinsama egrisi...
-fonk_numara = func_num;
+% Convergence Analysis
 FESindex = [0.01, 0.02, 0.03, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]*FES;
 
 % 1. Convergence Analysis
 % istenilen fonksiyonun (convergence curve) yakinsama egrisi...
 figure (2)
-semilogy(FESindex,solution(fonk_numara,1).t,'-db');
+semilogy(FESindex,solution(func_num,1).t,'-db');
 xlabel('Function Evaluations');
 ylabel('Error Value');
 str = sprintf('Convergence Analysis of FN%d',fonk_numara);
 title(str);
-
-% Optimization with MsRwGWO 
-
-[i_gbest,i_gbestval,i_FES,i_t]= MsRwGWO_func(fhd,D,pop_size,iter_max,Xmin,Xmax,X_suru,func_num);
-solution(func_num,1).position = i_gbest;
-solution(func_num,1).cost = i_gbestval-func_num*100;
-solution(func_num,1).t = i_t;
-fprintf('\n---------------------------------------------------------------\n');
-fprintf('Optimization with MsRwGWO\n ');
-fprintf('Func no: %d -> %d. run : best error = %1.2e\n',func_num,runs,solution(func_num,1).cost);
-
-% 1. Convergence Analysis
-% istenilen fonksiyonun (convergence curve) yakinsama egrisi...
-figure (2)
-hold on
-semilogy(FESindex,solution(fonk_numara,1).t,'-sr');
-legend('GWO','MsRwGWO')
